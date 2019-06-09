0e33daa04f8df120cf3ba0e98ac0f018b0fefb0d
Matthew Landauer <mlandauer@gmail.com>
2019-06-09 13:14:03 +1000

    Switch over to using gem

--- scraper.rb
+++ scraper.rb
@@ -1,33 +1,3 @@
-require 'mechanize'
-require 'scraperwiki'
+require "icon_scraper"
 
-# XML feed of the applications submitted in the last month
-base_url = "https://plan.gosford.nsw.gov.au/Pages/XC.Track"
-url = base_url + "/SearchApplication.aspx?o=xml&d=last14days&k=LodgementDate"
-
-agent = Mechanize.new
-page = Nokogiri::XML(agent.get(url).body)
-page.search("Application").each do |app|
-  # Some DAs have good descriptions whilst others just have
-  # "<insert here>" so we search for "<insert" and if it's there we
-  # use another more basic description
-  if app.at('ApplicationDetails').nil? || app.at('ApplicationDetails').inner_text.downcase.index(/(<|\()insert/)
-    description = app.at('NatureOfApplication').inner_text
-  else
-    description = app.at('ApplicationDetails').inner_text
-  end
-  council_reference = app.at('ReferenceNumber').inner_text
-
-  record = {
-    "council_reference" => council_reference,
-    "description" => description,
-    "date_received" => Date.parse(app.at('LodgementDate').inner_text).to_s,
-    # Only use the first address
-    "address" => app.at('Address Line1').inner_text + ", " + app.at('Address Line2').inner_text,
-    "date_scraped" => Date.today.to_s,
-    "info_url" => base_url + "/SearchApplication.aspx?id=" + council_reference
-  }
-
-  #p record
-  ScraperWiki.save_sqlite(['council_reference'], record)
-end
+IconScraper.scrape_and_save(:gosford)
